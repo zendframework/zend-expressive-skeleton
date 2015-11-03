1b622f29dab637685fa49f9e2c11a62aa9c4d2f6
Hari KT <kthari85@gmail.com>
2015-11-03 23:12:17 +0530

    Add functionality to make things faster with two stage configuration

--- src/Composer/Resources/config/container-aura-di.php
+++ src/Composer/Resources/config/container-aura-di.php
@@ -9,6 +9,20 @@ $config = require 'config.php';
 $builder = new ContainerBuilder();
 $container = $builder->newInstance();
 
+// Add config classes for two stage configuration
+// http://auraphp.com/blog/2014/04/07/two-stage-config/
+$configClasses = [
+    // 'Common',
+    // 'Dev',
+    // 'Prod',
+];
+$configs = [];
+foreach ($configClasses as $configClass) {
+    $conf = $container->newInstance($configClass);
+    $conf->define($container);
+    $configs[] = $conf;
+}
+
 // Inject config
 $container->set('config', $config);
 
@@ -23,4 +37,10 @@ foreach ($config['dependencies']['invokables'] as $name => $object) {
     $container->set($name, $container->lazyNew($object));
 }
 
+// Lock the container
+$container->lock();
+
+foreach ($configs as $conf) {
+    $conf->modify($container);
+}
 return $container;
